fc25b869f69a238fba867960077d82e40713a0d3
vikash kumar <vikashgolu550@gmail.com>
2020-11-12 22:05:58 +0530

    written first route

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const express   = require('express');
-const mongoose  = require('mongoose');
-const app       = express();
-const PORT      = process.env.PORT|| 8080
+const express    = require('express');
+const mongoose   = require('mongoose');
+const app        = express();
+const Driver     = require("./model/driver");
+const bodyParser = require('body-parser')
+const PORT       = process.env.PORT|| 8080
 mongoose.connect(process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/cab_driver_v1', {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//======================
+app.post('/api/v1/driver/register', (req, res) => {
+    var newdriver = new Driver({ name: req.body.name, email: req.body.email, phone_number: req.body.phone_number,license_number:req.body.license_number,car_number:req.body.car_no});
+    Driver.create(newdriver, function (err, data) {
+        if (err) {
+             res.send(err)
+        }
+        else {
+                res.send(data);
+            }
+        })
+    })
+
+
+
+
+
 
 
 app.listen(PORT,()=>
--- model/driver.js
+++ model/driver.js
@@ -1,11 +1,11 @@
 var mongoose = require("mongoose");
 
 var DriverSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    phone_number:Number,
-    license_number:String,
-    car_number:String
+    name: {type:String,unique:true,required:true},
+    email: {type:String,unique:true,required:true},
+    phone_number: {type:Number,unique:true,required:true},
+    license_number: {type:String,unique:true,required:true},
+    car_number:{type:String,unique:true,required:true},
 });
  
 
