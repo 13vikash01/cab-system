9f210922fe3af73295185fe9b95caf06c355fe20
vikash kumar <vikashgolu550@gmail.com>
2020-11-13 15:58:09 +0530

    written all the routes and made location model

--- app.js
+++ app.js
@@ -2,25 +2,191 @@ const express    = require('express');
 const mongoose   = require('mongoose');
 const app        = express();
 const Driver     = require("./model/driver");
+const Location   =require('./model/location')
 const bodyParser = require('body-parser')
 const PORT       = process.env.PORT|| 8080
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/cab_driver_v1', {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true});
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/cab_driver', {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-//======================
-app.post('/api/v1/driver/register', (req, res) => {
-    var newdriver = new Driver({ name: req.body.name, email: req.body.email, phone_number: req.body.phone_number,license_number:req.body.license_number,car_number:req.body.car_no});
+app.set("view engine","ejs");
+let cnt=0;
+
+app.post('/api/v1/driver/register/',(req,res)=>
+{
+    Driver.find({$or:[{email: req.body.email},{phone_number:req.body.phone_number},{license_number:req.body.license_number},{car_number:req.body.car_no}]}, 
+        (err, value)=> 
+ {
+    if (value.length>0)
+    {
+        
+        return res.status(400).json(
+            {
+                "status": "error",
+                "reason": "duplicate value"
+            } 
+         )
+    }
+    if(req.body.phone_number.length!=10)
+    {
+        return res.status(400).json(
+            {
+                "status": "error",
+                "reason": "phone number must be of 10 digit"
+            } 
+         )
+    }
+
+    
+
+ 
+    var newdriver = new Driver({id:++cnt, name: req.body.name, email: req.body.email, phone_number: req.body.phone_number,license_number:req.body.license_number,car_number:req.body.car_no});
     Driver.create(newdriver, function (err, data) {
         if (err) {
-             res.send(err)
+             res.status(400).json(
+                {
+                    "status": "failure",
+                    "reason": err
+                } 
+             )
         }
         else {
-                res.send(data);
+            res.status(201).json(
+                {
+                    "id": data.id,                   
+                    "name": data.name,
+                    "email": data.email,
+                    "phone_number":data.phone_number,
+                   "license_number": data.license_number,
+                    "car_number":  data.car_number
+                }
+            );
             }
         })
-    })
+    });
+})
 
+app.post('/api/v1/driver/:id/sendLocation/',(req,res)=>
+{
+      if(!req.body.latitude||!req.body.longitude)
+      {
+        res.status(400).json({ 
+            "status": "failure",
+            "reason": "longitude or latitude is null"
+        })
+      }
+    Location.create({id:req.params.id,latitude:req.body.latitude,longitude:req.body.longitude},(err,data1)=>
+    {
+            if(err)
+            {
+                res.status(400).json({ 
+                    "status": "failure",
+                    "reason": err
+                })
+            }
+            else
+            {
+                res.status(202).json({"status": "success"})
+            }
+    })
+    
+})
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2)
+      ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+  }
+  
+  function deg2rad(deg) {
+    return deg * (Math.PI/180)
+  }
+app.post('/api/v1/passenger/available_cabs/',(req,res)=>
+{
+     let lat=req.body.latitude
+     let long=req.body.longitude
+     let map=[]
+     Location.find({}, function(err, locations) {
+        
+       if(err)
+       {
+        res.status(400).json(
+            {
+                "status": "failure",
+                "reason": err
+            }
+        )
+       }
+       else
+       {
+        
+        locations.forEach(function(location) {
+           if(getDistanceFromLatLonInKm(lat,long,location.latitude.value,location.longitude.value)<=4)
+           {
+               map.push(location.id)
+           }
+           
+           
+        })
+        if(map.length>=1)
+        {
+        Driver.find(
+            {
+                id:{$in:map}},(err,data)=>
+                {
+                    if(err)
+                    {
+                        res.status(400).json(
+                            {
+                                "status": "failure",
+                                "reason": err
+                            }
+                        )
+                    }
+                    else
+                    {
+                        let ans=[];
+                        for(let i=0;i<data.length;i++)
+                        {
+                            let res={};
+                            res.name=data[i].name,
+                            res.car_number=data[i].car_number,
+                            res.phone_number=data[i].phone_number
+                            ans.push(res)
+                        }
+                        res.status(200).json(
+                            {
+                                "available_cabs":ans
+                            }
+                        )
+                    }
+                }
+            
+        )
+            }
+
+       else 
+       {
+             res.status(200).json(
+                 {
+                    "message": "No cabs available!"
+                 }
+             )
+       }
+
+      
+       }
+    
+          
+      });
+})
 
 
 
--- model/driver.js
+++ model/driver.js
@@ -1,12 +1,19 @@
-var mongoose = require("mongoose");
+  
+const mongoose=require('mongoose')
+require('mongoose-double')(mongoose)
 
-var DriverSchema = new mongoose.Schema({
-    name: {type:String,unique:true,required:true},
-    email: {type:String,unique:true,required:true},
-    phone_number: {type:Number,unique:true,required:true},
-    license_number: {type:String,unique:true,required:true},
-    car_number:{type:String,unique:true,required:true},
-});
- 
 
-module.exports = mongoose.model("Driver", DriverSchema);
+
+let driverSchema=new mongoose.Schema(
+   {
+       
+       id:{type:Number,default:0,unique:true},
+       name: {type:String,required:true,unique:true},
+       email: {type:String,required:true,unique:true},
+       phone_number: {type:Number,required:true,unique:true},
+       license_number: {type:String,required:true,unique:true},
+       car_number:{type:String,required:true,unique:true},
+       
+   }
+)
+module.exports = mongoose.model("Driver", driverSchema);
